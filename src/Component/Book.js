71d6cf812a95943a72672067200d766068c6db4d
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-09 22:59:06 +0500

    Update Book.js

--- src/Component/Book.js
+++ src/Component/Book.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const Book = (props) => {
   const { id, name, author } = props;
@@ -7,16 +7,15 @@ const Book = (props) => {
     <li className="list-item" key={id}>
       <p>{name}</p>
       <p>{author}</p>
-      <button type="button">Remove Book</button>
+      <button type="button">Remove book</button>
     </li>
-  )
-}
+  );
+};
 
 Book.propTypes = {
   id: PropTypes.func.isRequired,
   name: PropTypes.func.isRequired,
   author: PropTypes.func.isRequired,
-
 };
 
 export default Book;
