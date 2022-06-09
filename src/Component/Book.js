be0336be2874b17e05d420f77899bf79b8fe58a6
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-09 22:06:35 +0500

    Create Books.js

---
+++ src/Component/Book.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Book = (props) => {
+  const { id, name, author } = props;
+  return (
+    <li className="list-item" key={id}>
+      <p>{name}</p>
+      <p>{author}</p>
+      <button type="button">Remove Book</button>
+    </li>
+  )
+}
+
+Book.propTypes = {
+  id: PropTypes.func.isRequired,
+  name: PropTypes.func.isRequired,
+  author: PropTypes.func.isRequired,
+
+};
+
+export default Book;
