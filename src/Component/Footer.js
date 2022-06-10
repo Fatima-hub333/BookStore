dc0abd7adb2770e60e50d0aa9c117058a6ef7ed6
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-10 11:03:36 +0500

    Update Footer.js

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
   <div className="footer">
-    <p>Copyright @ 2022 Created by Adaobi</p>
+    <p>Copyright @ 2022 Created by Fatima</p>
   </div>
 );
 
