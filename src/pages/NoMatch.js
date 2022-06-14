3efcff089b0f5009d367694fab6421c79b56fd2e
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-14 22:17:28 +0500

    Delete files create issues...

--- src/pages/NoMatch.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const NoMatch = () => <h2>None</h2>;
-
-export default NoMatch;
--- src/pages/PageCatego.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Pagecategory = () => <button type="button">Check status</button>;
-
-export default Pagecategory;
