6655e8f694e4570581363ee0ad72a80eed2fb0fa
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-15 21:00:42 +0500

    Refactor addBook.js via using useState & useDispatch

--- src/Component/addBook.js
+++ src/Component/addBook.js
@@ -1,14 +1,1 @@
-import React from 'react';
 
-const BookAdd = () => (
-  <div>
-    <h3>Enter New Books</h3>
-    <form className="add-form">
-      <input type="text" name="title" placeholder="Book Title.." />
-      <input type="text" name="author" placeholder="Author.." />
-      <button type="submit">Add Book</button>
-    </form>
-  </div>
-);
-
-export default BookAdd;
